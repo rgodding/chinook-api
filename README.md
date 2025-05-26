8c63acafef9d3c641e8d54b18a2d91305f87933a
Rashaun Godding <rash0081@stud.kea.dk>
2025-05-27 01:01:28 +0200

    fixing deployment state

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # chinook-api
+
+/*
+DB_HOST=sql111.infinityfree.com
+DB_NAME=if0_39044140_chinookkea2025
+DB_USER=if0_39044140
+DB_PASSWORD=zApp3PJFwqMDxu
+*/
--- database/DBCredentials.php
+++ database/DBCredentials.php
@@ -2,14 +2,16 @@
 
 class DBCredentials
 {
-    protected string $host = 'localhost';
-    protected string $dbname = 'chinook_autoincrement';
-    protected string $user = 'root';
-    protected string $password = 'Skejs123';
+    protected string $host;
+    protected string $dbname;
+    protected string $user;
+    protected string $password;
+
+    public function __construct()
+    {
+        $this->host = getenv('DB_HOST') ?: 'localhost';
+        $this->dbname = getenv('DB_NAME') ?: 'chinook_autoincrement';
+        $this->user = getenv('DB_USER') ?: 'root';
+        $this->password = getenv('DB_PASSWORD') ?: '';
+    }
 }
-/*
-DB_HOST=sql111.infinityfree.com
-DB_NAME=if0_39044140_chinookkea2025
-DB_USER=if0_39044140
-DB_PASSWORD=zApp3PJFwqMDxu
-*/
