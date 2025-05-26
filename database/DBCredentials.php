581a0965490eefa382cf5ba4f2b9933b1c7186f7
Rashaun Godding <rash0081@stud.kea.dk>
2025-05-27 01:24:10 +0200

    fixing deployment state

--- database/DBCredentials.php
+++ database/DBCredentials.php
@@ -2,6 +2,8 @@
 
 class DBCredentials
 {
+    protected string $deployment;
+    protected string $port;
     protected string $host;
     protected string $dbname;
     protected string $user;
@@ -10,9 +12,12 @@ class DBCredentials
     public function __construct()
     {
         // Her sættes alle properties, så de er initialiserede
+        $this->deployment = getenv('DB_DEPLOYMENT') ?: 'local'; // Default to 'local' if not set
+        $this->port = getenv('DB_PORT') ?: '3306'; // Default MySQL port
         $this->host = getenv('DB_HOST') ?: 'localhost';
         $this->dbname = getenv('DB_NAME') ?: 'chinook_autoincrement';
         $this->user = getenv('DB_USER') ?: 'root';
         $this->password = getenv('DB_PASSWORD') ?: 'Skejs123';
+
     }
 }
