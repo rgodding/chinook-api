82146b56e05842354f84d01ce82164a868e3b2b7
Rashaun Godding <rash0081@stud.kea.dk>
2025-05-27 06:37:42 +0200

    removed logs from git

--- utils/logger.php
+++ utils/logger.php
@@ -8,12 +8,18 @@ function logMessage($message, $level = 'INFO')
     // Log to a file
     if($level === 'ERROR'){
         $logFile = BASE_PATH . '/logs/error.log';
+        if (!file_exists($logFile)) {
+            file_put_contents($logFile, ''); // Create the file if it doesn't exist
+        }
     } else {
         $logFile = BASE_PATH . '/logs/app.log';
+        if (!file_exists($logFile)) {
+            file_put_contents($logFile, ''); // Create the file if it doesn't exist
+        }
     }
+    
     file_put_contents($logFile, $logEntry, FILE_APPEND);
 }
-
 function logError($message)
 {
     logMessage($message, 'ERROR');
